a88e89c99c559aa06a807dac9e210706c1418f3a
Alberto <ajsaenz1091@gmail.com>
2021-05-26 12:12:37 -0400

    commit tree 5d89be4bcbc1160504513cc2d5ee4d21d89e55f3

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,6 +3,14 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
+
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     x:0,
+  //     y:0
+  //   }
+  // }
   
   handleMouseMove = (event) => {
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
@@ -10,23 +18,41 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+    // this.setState({x:event.nativeEvent.offsetX, y:event.nativeEvent.offsetY})
+    // drawChromeBoiAtCoords(this.state.x,this.state.y)
+    debugger;
+    let x = event.clientX;
+    let y = event.clientY;
+    drawChromeBoiAtCoords(x,y)
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-   
+   handleClick(){
+     toggleCycling();
+   }
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
+   handleKeyPress(e){
+    if(e.key === 'a'){
+      resize('+')
+    }else if(e.key === 's'){
+      resize('-')
+    }
+   }
   
   render() {
     return (
       <canvas 
+        onKeyPress={this.handleKeyPress}
+        onClick={this.handleClick}
         onMouseMove={this.handleMouseMove}
         width='900'
         height='600'
